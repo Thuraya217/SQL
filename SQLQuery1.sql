27f6ec04cd4f7bd54a47395523f6f3500015e1a8
Thuraiya AL Kalbani <147870686+Thuraya217@users.noreply.github.com>
2025-05-13 10:01:54 +0400

    translate company mapping to SQL table
    Translate ERD and mapped schemas into actual SQL code

---
+++ SQLQuery1.sql
@@ -0,0 +1,69 @@
+create database Company;
+
+use Company;
+
+create table Employees (
+Ssn int primary key,
+FName varchar (20) not null,
+Minit char (1) not null,
+LName varchar (20) not null,
+Bdate date check (Bdate <= GETDATE()),
+Address varchar ( 255 ),
+Sex char(1) CHECK (Sex IN ('M', 'F')),
+Salary decimal (10,2) check (Salary > 0 ),
+Super_ssn int,
+Dno int,
+foreign key (Super_ssn) references Employees (Ssn),
+foreign key (Dno) references department (Dnumber),
+);
+
+use Company;
+Go
+create table department (
+Dnumber int primary key,
+DName varchar (20) not null,
+Mgr_ssn int,
+Mgr_start_date date,
+foreign key (Mgr_ssn) references Employees (Ssn)
+);
+
+create table DEPT_LOCATIONS (
+Dnumber int,
+DLocation varchar (20),
+primary key (Dnumber , DLocation),
+foreign key (Dnumber) references department (Dnumber),
+);
+
+create table project (
+PNumber int primary key,
+PName varchar (20) not null,
+PLocation varchar (20),
+Dnum int
+foreign key (Dnum) references department (Dnumber),
+);
+
+create table Works_on (
+Essn int,
+Pno int,
+Hours decimal (5,2),
+primary key (Essn , Pno),
+foreign key (Essn) references Employees (Ssn),
+foreign key (Pno) references project (PNumber),
+);
+
+create table Dependent (
+Essn int,
+Dependent_name int,
+Sex char(1) check (Sex IN ('M', 'F')),
+Bdate date,
+Realtionship varchar (20),
+primary key (Essn , Dependent_name),
+foreign key (Essn) references Employees (Ssn),
+);
+
+alter table Employees 
+add DateOfBirth Date;
+
+alter table Employees
+drop column Minit;
+
